7ad7d3fa7b73b4398012faa2312c382c7f50df8d
benjaminpettus <benjaminpettus@gmail.com>
2016-03-08 21:47:33 -1000

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+
+
+
+
+var app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+
+
+
+
+var server = app.listen(3000, function() {
+  console.log('Listening to port', server.address().port);
+});
